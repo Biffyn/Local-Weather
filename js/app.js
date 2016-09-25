4ee70141c2f620a4cd38c516bf357bf3f3d73212
biffyn <nuravenweb@gmail.com>
2016-09-26 00:46:16 +0200

    Added display weather function

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
-    var lon,
-        lat,
-        apiKey = "f44d93e35972d2dc326a9b9c058f0f6d";        //enter API key here
-        // get geo-location data
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(function(position) {
-                lon = position.coords.longitude;
-    		    lat = position.coords.latitude;
-                var apiCall = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon="+ lon + "&units=metric&appid=" + apiKey;
-                // get local weather - Open Weather JSON
-                $.getJSON(apiCall, function(data){
-                    weather = data.weather[0].description;
-                    city = data.name;
-                    temp = data.main.temp.toFixed(0);
-                    console.log(temp);
-                });
-                //success
-                //error
+    var lon;
+    var lat;
+    var apiKey = "f44d93e35972d2dc326a9b9c058f0f6d";        //enter API key here
+    // get geo-location data
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lon = position.coords.longitude;
+		    lat = position.coords.latitude;
+            var apiCall = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon="+ lon + "&units=metric&appid=" + apiKey;
+            // get local weather - Open Weather JSON
+            console.log(apiCall);
+            $.getJSON(apiCall, function(res){
+                console.log(res);
+                // save what is needed from the response to vars
+                 id = res.weather[0].id;
+                 weather = res.weather[0].description;
+                 city = res.name;
+                 temp = res.main.temp.toFixed(0);
+                 // display weather
+                 $("#city").html(city);
+                 $("#temp").html(temp);
+                 $("#weather").html(weather);
             });
-            // get local weather
-            //success
-            //error
-        }
-
-    // display weather
+        });
+    }
         // change icon based on current weather
 
     // convert Fahrenheit to Celsius
-
-
 });
